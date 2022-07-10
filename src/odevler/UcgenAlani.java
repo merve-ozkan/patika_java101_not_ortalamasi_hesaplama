eac58072575782b4887b62d4461b06104a4c38a7
Merve Ozkan <merveozkan.dev@gmail.com>
2022-07-10 18:52:38 +0300

    first commit

--- src/odevler/UcgenAlani.java
+++ src/odevler/UcgenAlani.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 
 public class UcgenAlani {
     public static void main(String[] args) {
-
+        double a,b,c,u,alan;
+        Scanner kenar = new Scanner(System.in);
+        System.out.print("1.Kenar: ");
+        a = kenar.nextDouble();
+        System.out.print("2.Kenar: ");
+        b = kenar.nextDouble();
+        System.out.print("3.Kenar: ");
+        c = kenar.nextDouble();
+        u = (a+b+c)/2;
+        alan = Math.sqrt(((u*(u-a)*(u-b)*(u-c))));
+        System.out.println("Üçgenin alanı: " + alan);
     }
 }
