e0891b9797062fc28daeb578a77dd7a39f4cd517
Merve Ozkan <merveozkan.dev@gmail.com>
2022-07-11 04:48:59 +0300

    first_commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         int a;
-        Scanner input = new Scanner(System.in); // scanner sinifini kullanabilmek icin input degiskenine tanimladik
+        Scanner input = new Scanner(System.in);
         System.out.print("a sayisini giriniz: ");
         a = input.nextInt();
         System.out.println(a);
